8d37f6a12c9618442305591747c6de8e6898cc1b
nandanp11 <parabnandan111@gmail.com>
2023-01-07 11:44:49 +0530

    my new c file

---
+++ 19co34_exp2_p1.c
@@ -0,0 +1,16 @@
+//19co34   nandan parab
+//program to find sum of 2 nos
+
+#include<stdio.h>
+
+int main()
+{
+
+    int a,b,sum=0;      //declaration
+    printf("\n enter the vaues of a & b \t");
+    scanf("%d %d",&a,&b);       //accepting values
+    sum=a+b;       //calculation
+    printf("\n sum of %d and %d is %d",a,b,sum);
+
+    return(0);
+}
